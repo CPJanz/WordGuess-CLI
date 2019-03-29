27cac17f3dc3e181fdc0a181d1d74d3cd76d6049
Carl Janz <carl.janz@gmail.com>
2019-03-28 18:22:42 -0700

    Finished game logic, added quotes.txt file to hold quotes.

--- index.js
+++ index.js
@@ -1,29 +1,94 @@
 const Word = require("./word");
 const inquirer = require("inquirer");
-const PHRASE_LIST = ["It is a good day to die!", "The king is dead, long live the king.", "Life is like a box of chocolates"];
-const STARTING_LIVES = 15;
+const fs = require("fs");
+const STARTING_LIVES = 2;
+let phraseList = ["hi"]
 let lives;
 let gameWord;
 
-//TODO: Define a game
+// Read in the quotes stored in the quotes.txt file and process them.
+fs.readFile("quotes.txt", "utf8", function(error, data) {
+    if (error) { return console.log(error); };
+    phraseList = data.split("|");
+    game();
+});
+
 function game() {
     // Game starts with a random phrase being chosen and lives being set. 
-    gameWord = new Word(PHRASE_LIST[Math.floor(Math.random() * PHRASE_LIST.length)]);
+    gameWord = new Word(phraseList[Math.floor(Math.random() * phraseList.length)]);
     lives = STARTING_LIVES;
-    //Then begins taking turns until the game ends.
+    printNewGame();
     takeTurn();
 }
 
 function takeTurn() {
-    let promptString = gameWord.toString() + "\n\n Select a letter (case doesn't matter)."
+    let promptString = gameWord.toString() + "   Lives Remaining " + lives + " ||      Select a letter:";
+    // Shows current word status and prompts the player for a guess
     inquirer.prompt([{
-                    type: "input",
-                    name: "guessedLetter",
-                    message: promptString
+        type: "input",
+        name: "guessedLetter",
+        message: promptString,
+        filter: function(input) {
+            return input.toLowerCase();
+        },
+        validate: function(input) {
+            if (input.length === 1) {
+                if (/[a-z]/.test(input)) {
+                    if (!gameWord.alreadyGuessed(input)) {
+                        return true;
+                    }
+                    console.log(" That has already guessed!");
+                    return false;
                 }
+                console.log(" That is not a letter!");
+                return false;
             }
+            console.log(" Please input one letter!");
+            return false;
+        }
+    }]).then(function(input) {
+        // Prompt has cleaned input. Perform a guess, and decriment lives if it is an incorrect guess (no letters flipped).
+        if (gameWord.guess(input.guessedLetter)) {
+            // Correct guess, at least one letter was revealed by the guess.
+            if (gameWord.hasWon()) {
+                //Game has been won, display congratutlations and prompt for new game
+                console.log("You Won!");
+                promptForNewGame();
+            } else {
+                // Game has not been won, prompt for another letter.
+                takeTurn();
+            }
+        } else {
+            // Incorrect guess, (no letters were revealed by the guess). Decrement lives and check for game loss.
+            lives--;
+            if (lives > 0) {
+                takeTurn();
+            } else {
+                console.log("You Lost!")
+                promptForNewGame();
+            }
+        }
+    });
+}
 
+function promptForNewGame() {
+    let promptString = "Play again?";
+    inquirer.prompt([{
+        type: "confirm",
+        name: "newGame",
+        message: promptString
+    }]).then(function(answer) {
+        if (answer.newGame) {
+            game();
+        } else {
+            console.log("Farewell...");
+        }
+    });
+}
 
-            //TODO: Prompts the player for a guess
-            //TODO: Keep prompting until the player has guessed all letters or is out of guesses
-            //TODO: Prompt for another game
+function printNewGame() {
+    console.clear();
+    console.log("Welcome to quote guesser!\n\n");
+    console.log("I have cleverly hidden a quote, can you guess letters to reveal it before you run out of lives!?\n\n");
+    console.log("Good Luck!\n")
+}
--- word.js
+++ word.js
@@ -1,17 +1,17 @@
 const Letter = require("./letter")
 
 function Word(inputString) {
-    //Array of new letter objects representing the letters of the underlying word. This function is run on object creation and sets letters to the result.
+    /** Array of new letter objects representing the letters of the underlying word. This function is run on object creation and sets letters to the result.  */
     this.letters = (function() {
         let result = []
-            //Split the inputString into an array of letters
         inputString.split("").forEach(function(character) {
             result.push(new Letter(character));
         })
         return result;
     }())
     this.guessedLetters = [];
-    // Returns a string representing the word in it's current revealed state.
+
+    /**Returns a string representing the word in it's current revealed state.  */
     this.toString = function() {
         let result = ""
         this.letters.forEach(function(letter) {
@@ -19,22 +19,26 @@ function Word(inputString) {
         })
         return result;
     }
+
+    /** Takes in a potential guess, returns whether or not the letter has been guessed before  */
     this.alreadyGuessed = function(guess) {
-            return this.guessedLetters.includes(guess);
-        }
-        //Checks an input character against all letters in the array and updates any matching letters to revealed. Returns true if any letters are updated false if not.
+        return this.guessedLetters.includes(guess);
+    }
+
+    /** Checks an input character against all letters in the array and updates any matching letters to revealed. Returns true if any letters are updated false if not.  */
     this.guess = function(guessedLetter) {
-            let result = false;
-            this.guessedLetters.push(guessedLetter);
-            this.letters.forEach(function(letter) {
-                if (letter.guess(guessedLetter)) {
-                    result = true;
-                }
-            })
-            return result;
-        }
-        // Checks each letter for revealed status, returns true if all are revealed.
-    this.hasWon = function() {
+        let result = false;
+        this.guessedLetters.push(guessedLetter);
+        this.letters.forEach(function(letter) {
+            if (letter.guess(guessedLetter)) {
+                result = true;
+            }
+        })
+        return result;
+    }
+
+    /** Checks each letter for revealed status, returns true if all are revealed. */
+    this.hasWon = function(x) {
         let result = true;
         this.letters.forEach(function(letter) {
             if (!letter.revealed) {
